51cf7e771a00fb66e183c74e62c5d8204c9d2ea9
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-20 14:02:39 -0300

    [RFC][WIP] TEstes com hooks e base slim

--- escodoo_setup_base_br/hooks.py
+++ escodoo_setup_base_br/hooks.py
@@ -1,18 +1,34 @@
-# -*- coding: utf-8 -*-
+# Copyright 2024 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 import base64
 from odoo import api, tools, SUPERUSER_ID
 from odoo.modules.module import get_module_resource
 
 
 def _default_image(env):
+    """
+    Returns the default image encoded in base64 to be used for partners.
+
+    Args:
+        env: The Odoo environment for accessing resources.
+
+    Returns:
+        String: Default image encoded in base64.
+    """
     image_path = get_module_resource('escodoo_setup_base_br', 'static/img', 'res_partner_escodoo-image.png')
     return base64.b64encode(open(image_path, 'rb').read())
 
 
 def is_demo_data_installed(env):
     """
-    Checks if demo data is installed in the Odoo environment.
-    This is determined by checking if any module has demo data enabled.
+    Checks whether demo data is installed in the Odoo environment.
+
+    Args:
+        env: The Odoo environment for database access.
+
+    Returns:
+        Bool: True if demo data is installed, False otherwise.
     """
     demo_modules = env['ir.module.module'].search([('demo', '=', True)])
     return bool(demo_modules)
@@ -20,7 +36,11 @@ def is_demo_data_installed(env):
 
 def _load_partner_escodoo(cr, env):
     """
-    Loads the Escodoo partner if not already present in the database.
+    Loads the Escodoo partner into the database if not already present.
+
+    Args:
+        cr: Database cursor for executing SQL queries.
+        env: The Odoo environment for database access.
     """
     escodoo_partner = env['res.partner'].search(
         [('cnpj_cpf', '=', '03.684.524/0001-37')], limit=1)
@@ -37,7 +57,10 @@ def _load_partner_escodoo(cr, env):
 
 def _update_companies(env):
     """
-    Updates all companies with the Escodoo partner as technical support.
+    Updates all companies by setting the Escodoo partner as technical support.
+
+    Args:
+        env: The Odoo environment for database access.
     """
     escodoo_partner = env.ref(
         'escodoo_setup_base_br.partner_escodoo', raise_if_not_found=False)
@@ -49,6 +72,9 @@ def _update_companies(env):
 def _add_group_to_admin_user(env):
     """
     Adds the 'account.group_account_user' group to the admin user.
+
+    Args:
+        env: The Odoo environment for database access.
     """
     account_user_group = env.ref(
         'account.group_account_user', raise_if_not_found=False)
@@ -61,6 +87,9 @@ def _update_partners(env):
     """
     Updates the names and contact information of specific partners.
     First updates partners without a parent_id, then updates partners with a parent_id.
+
+    Args:
+        env: The Odoo environment for database access.
     """
     if is_demo_data_installed(env):
         target_partner_names = ['KMEE', 'Akretion', 'Engenere']
@@ -68,20 +97,20 @@ def _update_partners(env):
         filtered_partners = env['res.partner'].search(
             ['|'] * (len(domain) - 1) + domain)
 
-        # Primeiro atualiza parceiros pais
+        # First updates parent partners
         parent_index = 1
         for partner in filtered_partners.filtered(lambda p: not p.parent_id):
-            partner_name = f'Escodoo Empresa {parent_index}'
+            partner_name = f'Escodoo Company {parent_index}'
             partner.write({
                 'name': partner_name,
                 'legal_name': partner_name,
                 'website': 'https://www.escodoo.com.br',
-                'email': 'teste@escodoo.com.br',
+                'email': 'test@escodoo.com.br',
                 'image_1920': _default_image(env),
             })
             parent_index += 1
 
-        # Depois atualiza parceiros filhos
+        # Then updates child partners
         for partner in filtered_partners.filtered(lambda p: p.parent_id):
             parent_name = partner.parent_id.name
             partner_name = f'{parent_name} - {partner.state_id.name}'
@@ -89,7 +118,7 @@ def _update_partners(env):
                 'name': partner_name,
                 'legal_name': partner_name,
                 'website': 'https://www.escodoo.com.br',
-                'email': 'teste@escodoo.com.br',
+                'email': 'test@escodoo.com.br',
             })
     else:
         print('Skipping partner update because demo data is not installed.')
@@ -97,7 +126,11 @@ def _update_partners(env):
 
 def post_init_hook(cr, registry):
     """
-    Post-initialization hook for setting up module configuration.
+    Post-initialization hook for configuring module setup.
+
+    Args:
+        cr: Database cursor for executing SQL queries.
+        registry: Odoo database registry.
     """
     env = api.Environment(cr, SUPERUSER_ID, {})
     _load_partner_escodoo(cr, env)
