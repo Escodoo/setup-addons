c3a8b1ce3d83569e5f61fb7da525c2587bafb175
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-22 10:30:50 -0300

    [RFC][WIP] update chart

--- escodoo_setup_base_br/hooks.py
+++ escodoo_setup_base_br/hooks.py
@@ -16,7 +16,7 @@ def _default_image(env):
     Returns:
         String: Default image encoded in base64.
     """
-    image_path = get_module_resource('escodoo_setup_base_br', 'static/img', 'res_partner_escodoo-image.png')
+    image_path = get_module_resource('escodoo_setup_base_br', 'static/img', 'escodoo_icon.png')
     return base64.b64encode(open(image_path, 'rb').read())
 
 
@@ -55,6 +55,29 @@ def _load_partner_escodoo(cr, env):
         }])
 
 
+def _load_default_chart_of_accounts(env):
+    """
+    Carrega um plano de contas padrão para empresas que não têm um plano de contas.
+
+    Args:
+        env: O ambiente Odoo para acesso ao banco de dados.
+    """
+    # Identifica as empresas sem um plano de contas
+    companies_without_chart = env['res.company'].search([('chart_template_id', '=', False)])
+
+    # Carrega um plano de contas padrão (ajuste conforme necessário)
+    chart_template = env.ref('l10n_br_coa_generic.l10n_br_coa_generic_template', raise_if_not_found=False)
+    if not chart_template:
+        # Se o plano de contas padrão não for encontrado, retorne ou gere um erro conforme necessário
+        return
+
+    # Associa o plano de contas às empresas sem um
+    for company in companies_without_chart:
+        # Configura o ambiente com a empresa específica para carregar corretamente o plano de contas
+        env_cr = api.Environment(env.cr, company.id, env.context)
+        chart_template.with_env(env_cr).try_loading(company=company)
+
+
 def _update_companies(env):
     """
     Updates all companies by setting the Escodoo partner as technical support.
@@ -66,7 +89,57 @@ def _update_companies(env):
         'escodoo_setup_base_br.partner_escodoo', raise_if_not_found=False)
     if escodoo_partner:
         all_company_records = env['res.company'].search([])
-        all_company_records.write({'technical_support_id': escodoo_partner.id})
+        all_company_records.write(
+            {
+                'technical_support_id': escodoo_partner.id,
+            }
+        )
+
+
+# def _update_res_config_settings(env):
+#     """
+#     Updates the 'pwa_icon' field in the res.config.settings of the Odoo environment
+#     by creating or updating an attachment for the icon image.
+
+#     Args:
+#         env: The Odoo environment for database access.
+#     """
+#     # Nome do anexo para identificação
+#     attachment_name = "escodoo_pwa_icon.png"
+
+#     # Obter a imagem padrão codificada em base64
+#     image_base64 = _default_image(env)
+
+#     # Busca por um anexo existente com o mesmo nome
+#     existing_attachment = env['ir.attachment'].search([('name', '=', attachment_name)], limit=1)
+
+#     # Se não existir um anexo, cria um novo
+#     if not existing_attachment:
+#         # Cria o anexo no Odoo
+#         attachment = env['ir.attachment'].create({
+#             'name': attachment_name,
+#             'type': 'binary',
+#             'datas': image_base64,
+#             'store_fname': attachment_name,
+#             'mimetype': 'image/png',
+#         })
+
+#         # Obtém a URL do anexo
+#         attachment_url = f'/web/content/{attachment.id}/{attachment_name}'
+#     else:
+#         # Se já existir um anexo, atualiza com a nova imagem
+#         existing_attachment.write({'datas': image_base64})
+#         # Obtém a URL do anexo existente
+#         attachment_url = f'/web/content/{existing_attachment.id}/{attachment_name}'
+
+#     # Obtém o registro de configurações atuais
+#     config_settings = env['res.config.settings'].create({})
+
+#     # Escreve a URL do anexo no campo 'pwa_icon' das configurações
+#     config_settings.write({'pwa_icon': attachment_url})
+
+#     # Aplica as configurações
+#     config_settings.execute()
 
 
 def _add_group_to_admin_user(env):
@@ -135,5 +208,17 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     _load_partner_escodoo(cr, env)
     _update_companies(env)
+    # _update_res_config_settings(env)
+    _load_default_chart_of_accounts(env)
     _add_group_to_admin_user(env)
     _update_partners(env)
+
+    env["ir.config_parameter"].set_param("auth_signup.invitation_scope", "b2b")
+    env["ir.config_parameter"].set_param("pwa.manifest.background_color", "#7C7BAD")
+    env["ir.config_parameter"].set_param("pwa.manifest.name", "Odoo Escodoo")
+    env["ir.config_parameter"].set_param("pwa.manifest.theme_color", "#7C7BAD")
+    env["ir.config_parameter"].set_param("support_company", "Escodoo Sistemas")
+    env["ir.config_parameter"].set_param("support_company_url", "https://www.escodoo.com.br")
+    env["ir.config_parameter"].set_param("support_branding_color", "#7C7BAD")
+    env["ir.config_parameter"].set_param("support_email", "suporte@escodoo.com.br")
+    env["ir.config_parameter"].set_param("support_release", "14.0")
