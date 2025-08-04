ce31f4c74fd05a707dfddc3ac6255f8db2c8843b
Marcel Savegnago <marcel.savegnago@gmail.com>
2025-08-04 14:41:46 -0300

    [IMP] escodoo_setup_base: black, isort, prettier

--- escodoo_setup_base/__manifest__.py
+++ escodoo_setup_base/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
-    "website": "https://github.com/Escodoo/base-addons",
+    "website": "https://github.com/Escodoo/setup-addons",
     "depends": [
         "account_analytic_required",
         "account_asset_management",
